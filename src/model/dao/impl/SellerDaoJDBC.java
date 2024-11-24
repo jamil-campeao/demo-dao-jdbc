f273ef0a833d0fc3f5e3c83adabc0324b484104a
jamil-campeao <134243268+jamil-campeao@users.noreply.github.com>
2024-11-24 18:06:36 -0300

    FindAll implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -78,8 +78,45 @@ public class SellerDaoJDBC implements SellerDao {
 	
 	@Override
 	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(" SELECT               	  "+
+									   " S.*,                     "+
+									   " D.NAME AS DepName        "+
+									   " FROM SELLER S            "+
+									   " INNER JOIN DEPARTMENT D  "+
+									   " ON D.ID = S.DEPARTMENTID "+
+									   " ORDER BY NAME            "
+									   );
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();
+			
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				
+				list.add(obj);
+			}
+			return list;
+			
+		} 
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
